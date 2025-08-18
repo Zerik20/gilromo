a14d59ed2dcc42d036d3ac40f559f2bda33872d1
Zerik20 <erikaban14@gmail.com>
2025-08-17 19:00:18 -0600

    Hasta aqui todo bien en lo principal

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,14 @@
+// src/components/Icons/index.ts
+export { default as UserSolid } from "./UserSolid.astro";
+export { default as ClockSolid } from "./ClockSolid.astro";
+export { default as Github } from "./Github.astro";
+export { default as Facebook } from "./Facebook.astro";
+export { default as Location } from "./Location.astro";
+export { default as Envelope } from "./Envelope.astro";
+export { default as PaperPlane } from "./PaperPlane.astro";
+export { default as Message } from "./Message.astro";
+export { default as Phone } from "./Phone.astro";
+export { default as Whatsapp } from "./Whatsapp.astro";
+export { default as Instagram } from "./Instagram.astro";
+export { default as Tiktok } from "./Tiktok.astro";
+// ... other icons
